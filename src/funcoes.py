fb830d6f00aaa03ef9323503fc4377b2ace05ac1
unknown <viniciustavaresmendes051@gmail.com>
2024-10-01 17:30:15 -0300

    terceiro data set (complemento - episodes)

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import csv
+import re
 
 def carregar_dados(filename: str):
     data_frame_dados = pd.read_csv(filename, sep=',')
     return data_frame_dados
 
-def filtrar_generos(cabecalho, dados):
+def filtrar_generos(cabecalho, dados: pd.DataFrame):
     # Gêneros que queremos remover
     generos_indesejados = ['hentai', 'yaoi', 'yuri', 'ecchi', 'shota', 'loli', 'harem', 'reverse harem', 'josei']
     
@@ -17,11 +19,17 @@ def filtrar_generos(cabecalho, dados):
     
     return dados_filtrados
 
-def limpar_dados(data_frame, colunas = False):
-    data_frame = data_frame.drop_duplicates() 
-    if colunas:
-        data_frame.drop(colunas, axis=1, inplace=True)
-    return data_frame
+def limpar_dados(df: pd.DataFrame):
+    # Remove linhas onde 'type' está vazio
+    df = df[df['type'].notna()]
+    # Atualiza 'episodes' para 1 onde 'type' é 'Movie' e 'episodes' é 'unknown'
+    df.loc[(df['type'] == 'Movie') & (df['episodes'] == 'unknown'), 'episodes'] = 1
+    # Remove linhas onde 'rating', 'genre', 'members' ou 'aired' estão vazios
+    df = df[df['rating'].notna()]
+    df = df[df['genre'].notna()]
+    df = df[df['members'].notna()]
+    df = df[df['aired'].notna()]
+    return df
 
 def trocar_index(data_frame: pd.DataFrame, novos_index: dict):
     data_frame.rename(columns= novos_index, inplace=True)
@@ -31,6 +39,26 @@ def selecionar_colunas(data_frame: pd.DataFrame, index):
     filtred_data_frame = data_frame[index]
     return filtred_data_frame
 
-def completar_dados(data_frame_principal: pd.DataFrame, data_frame_complementar: pd.DataFrame, index: str, valor: str):
-    data_frame_principal[index,valor].fillna(data_frame_complementar.loc[index,valor])
-    return data_frame_principal
+
+def salvar_anime_csv(cabecalho, dados: pd.DataFrame, caminho='anime_filtrado.csv'):
+    try:
+        with open(caminho, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(cabecalho)  # Escreve o cabeçalho
+            writer.writerows(dados)      # Escreve os dados filtrados
+        print(f"Os dados filtrados foram salvos em '{caminho}'.")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo: {e}")
+
+def convert_date(date_str):
+    match = re.match(r'(\w{3}) (\d{1,2}), (\d{4})', date_str)
+    if match:
+        month_str, day, year = match.groups()
+        month_mapping = {
+            'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
+            'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
+            'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
+        }
+        month = month_mapping.get(month_str)
+        return f"{day.zfill(2)}/{month}/{year}"
+    return None
--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
-from data_functions import *
+from funcoes import *
 
 
+data_frame = carregar_dados('./data/dados_completos.csv')
+data_frame = data_frame[data_frame['episodes'] != 'Unknown']
+data_frame['episodes'] = pd.to_numeric(data_frame['episodes'], errors='coerce')
+#data_frame = data_frame[data_frame['episodes'] != np.nan]
+'''print(data_frame['episodes'].dtype)
+print(data_frame['episodes'].max())'''
 
-data_frame_principal = carregar_dados('./data/anime.csv')
-data_frame_complementar = carregar_dados('./data/animes_complemento.csv')
-data_frame_principal = limpar_dados(data_frame_principal)
-data_frame_complementar = limpar_dados(data_frame_complementar)
-
-novos_index ={
-    'anime_id':'uid',
-    'name':'title',
-    'rating':'score'
-}
-data_frame_principal = trocar_index(data_frame_principal, novos_index)
-data_frame_complementar = selecionar_colunas(data_frame_complementar, ['uid','title','genre','episodes','score','members'])
-print(data_frame_principal.columns)
-
-#data_frame_completo = pd.merge(data_frame_principal,data_frame_complementar, on='episodes',how='outer')
-#print(data_frame_completo.head)
