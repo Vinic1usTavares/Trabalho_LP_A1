e98421f6fe255d87fb7cea14d7010d3578a86065
unknown <viniciustavaresmendes051@gmail.com>
2024-10-10 01:30:40 -0300

    documentação das hipótese e das funções

--- src/Hipotese2.py
+++ src/Hipotese2.py
@@ -1,27 +1,34 @@
-from funcoes import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
+# Abrindo o data frame
 data_frame = pd.read_csv('./data/anime_filtrado_limpo.csv',encoding='ISO-8859-1',sep=',')
+
+# Convertendo o tipo da coluna episódios para numérico
 data_frame['episodes'] = pd.to_numeric(data_frame['episodes'], errors='coerce')
 data_frame= data_frame[data_frame['episodes'] != np.nan]
-print(data_frame['episodes'].dtype)
-print(data_frame['episodes'].max())
-print(data_frame['episodes'].min())
+
+# Informações básicas do Data Frame
+print(f"valor máximo da coluna de episódios: {data_frame['episodes'].max()}")
+print(f"valor mínimo da coluna de episódios: {data_frame['episodes'].min()}")
+
+# Discretizando os valores 
+
 labels = ['A','B','C','D','E','F','G','H','I']
 bins_episodes = [0,100,200,300,400,500,600,1000,1500,2000]
-index = ['name','episodes','rating','members']
+index = ['name','episodes','rating','members', 'type']
 selected_data = data_frame[index].copy()
-movies= len(selected_data[selected_data['episodes']== 1])
 
+movies= len(selected_data[selected_data['type'] == "Movie"])
 print(f"quantidade de filmes: {movies} \n")
 
 selected_data['episodes'] = pd.cut(selected_data['episodes'] ,bins_episodes, labels=labels)
 
 
-# gerando informação para ser explorada:
+# Gerando informação para ser explorada:
 
 print(selected_data.groupby('episodes').count())
-
 
 rating_media = selected_data.groupby('episodes')['rating'].mean()
 rating_max = selected_data.groupby('episodes')['rating'].max()
@@ -29,8 +36,10 @@ rating_min = selected_data.groupby('episodes')['rating'].min()
 rating_mediana = selected_data.groupby('episodes')['rating'].median()
 
   
-# Gráfico: Episódios x Número de membros
 
+# Gera informação para analizar a relação entre a quantidade de episódios de uma obra e o número de membros
+
+# Gráfico: Episódios x Número de membros
 plt.figure(figsize=(10, 6))
 plt.scatter(data_frame['episodes'], data_frame['members'], alpha=0.5)
 plt.title('Episódios x Número de membros')
@@ -40,12 +49,12 @@ plt.grid(True)
 plt.savefig('.\docs1\dados_hipotese2\EpisódiosxNúmero de membros.png') # Salvar o gráfico
 plt.close() # Fechar a figura para evitar conflitos
 
-  
 
-# Análise para episódios >= 500
+# Análise detalhada de episódios X membros para episódios >= 500
+df_more_episodes = data_frame[(data_frame['episodes'] >= 500) & (data_frame['members'] < 100000)].copy() # members <100 000 para não contabilizar "outliners"
+media = df_more_episodes['rating'].mean() # média de rating dos episódios com valor >= 500
 
-df_more_episodes = data_frame[(data_frame['episodes'] >= 500) & (data_frame['members'] < 100000)].copy()
-media = df_more_episodes['rating'].mean()
+# Geração do gráfico
 plt.figure(figsize=(10, 6))
 plt.scatter(df_more_episodes['episodes'], df_more_episodes['members'], alpha=0.5)
 plt.title(f'Episódios(500+) x Número de membros (media:{media})')
@@ -56,6 +65,7 @@ plt.savefig('.\docs1\dados_hipotese2\Episódios(500+)xNúmero de membros.png') #
 plt.close() # Fechar a figura para evitar conflitos
 
   
+# Gera informação para análise da relação entre número de episódios e rating de uma obra
 
 # Gráfico: Episódios x Rating
 plt.figure(figsize=(10, 6))
@@ -71,15 +81,17 @@ plt.close() # Fechar a figura para evitar conflitos
 
 # Análise para episódios < 125
 
-df_more_episodes = data_frame[(data_frame['episodes'] >= 500) & (data_frame['members'] < 100000)].copy()
-media = df_more_episodes['rating'].mean()
+df_less_episodes = data_frame[data_frame['episodes'] <= 125].copy() # Seleção dos episódios com valor menor ou igual a 125
+media = df_less_episodes['rating'].mean() # média dos ratings
+
+# Geração do Gráfico
 plt.figure(figsize=(10, 6))
-plt.scatter(df_more_episodes['episodes'], df_more_episodes['members'], alpha=0.5)
-plt.title(f'Episódios(500+) x Número de membros (media:{media})')
-plt.xlabel('Episódios(500+)')
+plt.scatter(df_less_episodes['episodes'], df_less_episodes['members'], alpha=0.5)
+plt.title(f'Episódios(125-) x Número de membros (media:{media})')
+plt.xlabel('Episódios(125-)')
 plt.ylabel('Número de membros')
 plt.grid(True)
-plt.savefig('.\docs1\dados_hipotese2\Episódios(500+)xNúmero de membros.png') # Salvar o gráfico
+plt.savefig('.\docs1\dados_hipotese2\Episódios(125-)xNúmero de membros.png') # Salvar o gráfico
 plt.close() # Fechar a figura para evitar conflitos
 
   
@@ -90,7 +102,6 @@ index = ['name','episodes']
 selected_data2 = data_frame[index].copy()
 
 # Discretizando valores
-
 labels = ['0-20','101-200','201-300','301-400','401-200']
 bins_episodes = [0,20,50,100,400,2000]
 selected_data2['episodes_range'] = pd.cut(selected_data2['episodes'] , bins=bins_episodes, right=False)
@@ -102,8 +113,6 @@ counts = selected_data2['episodes_range'].value_counts().sort_index()
 plt.figure(figsize=(10, 6))
 bars = counts.plot(kind='bar', color='skyblue')
 
-  
-
 # Adicionando anotações com a quantidade em cada barra
 for bar in bars.patches:
     bars.annotate(
@@ -113,7 +122,7 @@ for bar in bars.patches:
     va='bottom'  # Alinhamento vertical
     )
 
-  
+# Gera dados para analizar a quantidade de obras por faixa de episódios
 plt.title('Quantidade de Animes por Faixa de Episódios')
 plt.xlabel('Faixa de Episódios')
 plt.ylabel('Quantidade de Animes')
